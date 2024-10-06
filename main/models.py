08692a1f41e4b1dd278bc6cc19af0d3cab65e7eb
Chromss <nabilrahman330@gmail.com>
2024-10-06 19:40:13 +0700

    Test

--- main/models.py
+++ main/models.py
@@ -1,3 +1,3 @@
-from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import *
+from django.db import *
+import uuid
--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import datetime as d
+from django.shortcuts import *
+from django.contrib import *
+from django.contrib.auth import *
+from django.contrib.auth.forms import *
+from django.contrib.auth.decorators import *
+from django.core import *
+from django.http import *
+from django.views.decorators.csrf import *
+from django.views.decorators.http import*
+from django.utils.html import *
+from main.forms import *
+from main.models import *
 
-# Create your views here.
+def landing_page(request):
+    return render(request, 'landing_page.html')
