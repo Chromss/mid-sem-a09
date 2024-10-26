5330ef287e59888bd598ca984797cf054bd0a40b
malikaatha <malika.atha@ui.ac.id>
2024-10-26 12:01:01 +0700

    coll blm bisa diklik

--- placeCollection/urls.py
+++ placeCollection/urls.py
@@ -6,4 +6,6 @@ app_name = 'placeCollection'
 urlpatterns = [
     path('show-collections/', views.show_collections, name='show_collections'),
     path('create/', views.create_collection, name='create_collection'),
+    path('collections/<int:collection_id>/places/', views.show_collection_places, name='show_collection_places'),  # URL baru
+
 ]
--- placeCollection/views.py
+++ placeCollection/views.py
@@ -5,6 +5,18 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from .forms import CollectionForm
 from .models import Collection
+from django.shortcuts import render, get_object_or_404
+# from .models import Place  # Asumsi model Place sudah ada nantinya
+
+def show_collection_places(request, collection_id):
+    collection = get_object_or_404(Collection, id=collection_id)
+    # places = Place.objects.filter(collection=collection)  # Asumsi akan ada relasi ke Collection
+    context = {
+        'collection': collection,
+        # 'places': places,
+    }
+    return render(request, "collection_places.html", context)
+
 
 
 def show_collections(request):
