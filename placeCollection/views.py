0eb60ef8486ea96c8a4d16f39313a78d1f74ca13
patriciagloria <patglo2005@gmail.com>
2024-10-27 08:16:31 +0700

    add ajalah

--- placeCollection/views.py
+++ placeCollection/views.py
@@ -1,6 +1,6 @@
 # views.py
 from django.shortcuts import render, get_object_or_404
-from .models import PlaceCollection
+from .models import Collection
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
@@ -9,10 +9,10 @@ from django.views.decorators.http import require_http_methods
 @require_http_methods(["DELETE"])
 def delete_collection(request, collection_id):
     try:
-        collection = PlaceCollection.objects.get(id=collection_id)
+        collection = Collection.objects.get(id=collection_id)
         collection.delete()
         return JsonResponse({'success': True})
-    except PlaceCollection.DoesNotExist:
+    except Collection.DoesNotExist:
         return JsonResponse({'success': False, 'error': 'Collection not found'}, status=404)
     except Exception as e:
         return JsonResponse({'success': False, 'error': str(e)}, status=500)
@@ -23,7 +23,7 @@ def create_collection(request):
         name = request.POST.get('name')
         if name:
             try:
-                collection = PlaceCollection.objects.create(name=name)
+                collection = Collection.objects.create(name=name)
                 created_at = collection.created_at.strftime('%b %d, %Y')
                 return JsonResponse({
                     'success': True,
@@ -37,14 +37,14 @@ def create_collection(request):
     return JsonResponse({'success': False, 'error': 'Invalid request method'})
 
 def show_collections(request):
-    collections = PlaceCollection.objects.all()
+    collections = Collection.objects.all()
     return render(request, "collection.html", {
         'collections': collections,
     })
 
 
 def show_collection_places(request, collection_id):
-    collection = get_object_or_404(PlaceCollection, id=collection_id)
+    collection = get_object_or_404(Collection, id=collection_id)
     places = collection.places.all()
     return render(request, 'collection_places.html', {
         'collection': collection,
