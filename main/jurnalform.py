e9f7c94292e53ab9c1d3a2ba2e92efe1c5f79291
NurKhoirunnisaSalsabila <nur.khoirunnisa@ui.ac.id>
2024-10-27 21:57:55 +0700

    push master

--- main/jurnalform.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Journal
-
-class JournalForm(forms.ModelForm):
-    class Meta:
-        model = Journal
-        fields = ['title', 'content', 'image']
--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ PRODUCTION = os.getenv("PRODUCTION", False)
 
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nur-khoirunnisa-mlakumlaku.pbp.cs.ui.ac.id"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
