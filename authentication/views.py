e68965968a7bb1a6c69961cbbdf850988a7aceb1
NurKhoirunnisaSalsabila <nur.khoirunnisa@ui.ac.id>
2024-12-22 20:01:45 +0700

    udh merge

--- authentication/views.py
+++ authentication/views.py
@@ -5,9 +5,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.middleware.csrf import get_token
 
 
-# from django.contrib.auth.models import User
-# import json
-# from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.models import User
+import json
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 @csrf_exempt
@@ -94,57 +94,57 @@ def login(request):
 #             "message": "Login gagal, periksa kembali email atau kata sandi."
 #         }, status=401)
 
-# @csrf_exempt
-# def register(request):
-#     if request.method == 'POST':
-#         data = json.loads(request.body)
-#         username = data['username']
-#         password1 = data['password1']
-#         password2 = data['password2']
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data['username']
+        password1 = data['password1']
+        password2 = data['password2']
 
-#         # Check if the passwords match
-#         if password1 != password2:
-#             return JsonResponse({
-#                 "status": False,
-#                 "message": "Passwords do not match."
-#             }, status=400)
+        # Check if the passwords match
+        if password1 != password2:
+            return JsonResponse({
+                "status": False,
+                "message": "Passwords do not match."
+            }, status=400)
         
-#         # Check if the username is already taken
-#         if User.objects.filter(username=username).exists():
-#             return JsonResponse({
-#                 "status": False,
-#                 "message": "Username already exists."
-#             }, status=400)
+        # Check if the username is already taken
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({
+                "status": False,
+                "message": "Username already exists."
+            }, status=400)
         
-#         # Create the new user
-#         user = User.objects.create_user(username=username, password=password1)
-#         user.save()
+        # Create the new user
+        user = User.objects.create_user(username=username, password=password1)
+        user.save()
         
-#         return JsonResponse({
-#             "username": user.username,
-#             "status": 'success',
-#             "message": "User created successfully!"
-#         }, status=200)
+        return JsonResponse({
+            "username": user.username,
+            "status": 'success',
+            "message": "User created successfully!"
+        }, status=200)
     
-#     else:
-#         return JsonResponse({
-#             "status": False,
-#             "message": "Invalid request method."
-#         }, status=400)
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Invalid request method."
+        }, status=400)
     
-# @csrf_exempt
-# def logout(request):
-#     username = request.user.username
+@csrf_exempt
+def logout(request):
+    username = request.user.username
 
-#     try:
-#         auth_logout(request)
-#         return JsonResponse({
-#             "username": username,
-#             "status": True,
-#             "message": "Logout berhasil!"
-#         }, status=200)
-#     except:
-#         return JsonResponse({
-#         "status": False,
-#         "message": "Logout gagal."
-#         }, status=401)
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logout berhasil!"
+        }, status=200)
+    except:
+        return JsonResponse({
+        "status": False,
+        "message": "Logout gagal."
+        }, status=401)
